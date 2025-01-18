7884e9396732e668471919c113f92b4c1f0503ff
Jonathan Lin <jlin@winteroftext.ca>
2025-01-18 09:53:31 -0800

    add scoping example and re-style

--- refresher1.c
+++ refresher1.c
@@ -4,7 +4,10 @@
 
 #define GET_NAME(var) printf("%s = %d\n", #var, var)
 
-int main() {
+void scope_example();
+
+int main()
+{
 
 	extern int32_t x;
 	extern float z;
@@ -12,10 +15,34 @@ int main() {
 	uint8_t i = 10;
 
 	GET_NAME(i);
+    scope_example();
 
 	return 0;
 }
 
-int swap(int a, int b){
+int swap(int a, int b)
+{
     return 0;
+}
+
+void f1()
+{
+    int i = 0;    // local variables, exists until block ends
+                  // once block ends, variable is destroyed
+    char a,b[10];
+    while (i>0)
+    {
+        int x;    // not intializing inside the while, but
+    }
+}
+
+void scope_example()
+{
+    int num = 7;
+    {
+        int num = 10;
+        num = num + 10;
+        printf ("num = %d\n", num);
+    }
+    printf ("external num = %d\n", num);
 }
